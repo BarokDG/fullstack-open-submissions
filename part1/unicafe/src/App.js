fc41ac984b8341671a1f4c280f657a3f0c12628e
Barok Dagim <barok@garrilogistics.com>
2023-06-08 13:07:07 +0300

    Exercise 1.9✅

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -15,12 +15,18 @@ const Statistics = (props) => {
     <>
       <h1>Statistics</h1>
 
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {total}</p>
+          <p>average {average}</p>
+          <p>positive {positive}%</p>
+        </>
+      )}
     </>
   );
 };
