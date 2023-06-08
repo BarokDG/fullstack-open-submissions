a24fa59818e1a98149e9ebb01fdb314cd339c9f1
Barok Dagim <barok@garrilogistics.com>
2023-06-08 12:59:59 +0300

    Exercise 1.7✅

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const total = good + neutral + bad;
+  const average = (good + bad * -1) / total;
+  const positive = (good / total) * 100 + "%";
+
   return (
     <>
       <h1>Give feedback</h1>
@@ -19,6 +23,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {total !== 0 ? average : "N/A"}</p>
+      <p>positive {total !== 0 ? positive : "N/A"}</p>
     </>
   );
 };
